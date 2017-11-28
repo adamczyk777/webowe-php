8f3c9f514f65fb7e7ebc825fa7f00684890d56d7
Jakub Adamczyk <adamczyk777@gmail.com>
2017-11-28 17:35:38 +0100

    zajecia

--- wpis.php
+++ wpis.php
@@ -9,6 +9,13 @@ $file1 = $_POST['file1'];
 $file2 = $_POST['file2'];
 $file3 = $_POST['file3'];
 
-
-
+$blogs = scandir("./blogs");
+foreach($blogs as $blogfile) {
+    $file = fopen("./".$blogfile."/info.txt");
+    $local_uname = fgets($file);
+    $local_pass = md5(fgets($file));
+    if($local_uname == $username && $local_pass == $password) {
+        $newfile = fopen('./blogs/'.$blogfile.'/'.$date.'.blg', 'w');
+        file_put_contents('./blogs/'.$blogfile.'/'.$date.'.blg', "heythere");
+}
 ?>
