47499d43685fe5308022ef4a4794ed87c31b5c40
Jakub Adamczyk <adamczyk777@gmail.com>
2017-11-29 01:28:41 +0100

    opening certain blogs working

--- wpis.php
+++ wpis.php
@@ -2,20 +2,36 @@
 
 //post data
 $username = $_POST['username'];
+print_r($username."<br>");
 $password = md5($_POST['password']);
+print_r($password."<br>");
+print_r($_POST['password']."<br>");
 $date = $_POST['date'];
 $time = $_POST['time'];
 $file1 = $_POST['file1'];
 $file2 = $_POST['file2'];
 $file3 = $_POST['file3'];
 
-$blogs = scandir("./blogs");
-foreach($blogs as $blogfile) {
-    $file = fopen("./".$blogfile."/info.txt");
-    $local_uname = fgets($file);
-    $local_pass = md5(fgets($file));
-    if($local_uname == $username && $local_pass == $password) {
-        $newfile = fopen('./blogs/'.$blogfile.'/'.$date.'.blg', 'w');
-        file_put_contents('./blogs/'.$blogfile.'/'.$date.'.blg', "heythere");
+$blogs = scandir("blogs/");
+foreach($blogs as $path) {
+    if($path != "." && $path != ".." && is_dir("blogs/" . $path)) {
+        if($file = fopen("blogs/".$path."/info.txt", 'r')) {
+            $line1 = trim(fgets($file));
+            print_r($line1."<br>");
+            $line2 = trim(fgets($file));
+            print_r($line2."<br>");
+            if ($line1 == $username && $line2 == $password) {
+                //tu tworzymyplik
+            }
+        }
+    }
+    print_r('koniec'."<br>");
+    
+    // $local_uname = fgets($file);
+    // $local_pass = md5(fgets($file));
+    // if($local_uname == $username && $local_pass == $password) {
+    //     $newfile = fopen("blogs/".$blogfile."/".$date."blg", 'w');
+    //     file_put_contents('blogs/'.$blogfile.'/'.$date.'.blg', "heythere");
+    // }
 }
 ?>
